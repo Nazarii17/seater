1b1082ba56d354e3a284c1364f006c568028bfdc
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-20 00:37:43 +0200

    added many to many, userinfo in message for differenta users

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -20,9 +20,17 @@ public class Message {
     private String text;
     @Column(name = "tag")
     private String tag;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
+    }
+
+    public String getAuthorName(){
+        return author !=null ? author.getUsername() : "<none>";
     }
 }
