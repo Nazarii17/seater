7bc1eb3cff822f0e4366ae0c4f4e35602ce80ffa
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-20 19:59:43 +0200

    changed ui to bootstrap

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
     @Value("${upload.path}")
     private String uploadPath;
 
@@ -18,7 +19,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
-                .addResourceLocations("file:///"+uploadPath+"/");
+                .addResourceLocations("file:///" + uploadPath + "/");
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
